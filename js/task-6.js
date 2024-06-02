b7a656f35c5ced47d36f951fd36df4689ef0c1ab
Lena-Stefaniuk <157167812+Lena-Stefaniuk@users.noreply.github.com>
2024-06-02 13:12:15 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -8,16 +8,22 @@ const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
 const boxesContainer = document.querySelector("#boxes");
 
-createBtn.addEventListener("click", createBoxes);
+createBtn.addEventListener("click", handleCreateBoxes);
 destroyBtn.addEventListener("click", destroyBoxes);
 
-function createBoxes() {
+function handleCreateBoxes() {
   const amount = input.value;
 
   if (amount < 1 || amount > 100) {
     alert("Please enter a number between 1 and 100.");
     return;
   }
+
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  boxesContainer.innerHTML = "";
 
   for (let i = 0; i < amount; i++) {
     const box = document.createElement("div");
