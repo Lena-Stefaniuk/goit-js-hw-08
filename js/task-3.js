d5dbe5d765b1abddeb7c8ddb0c548a63ed4d5864
Lena-Stefaniuk <157167812+Lena-Stefaniuk@users.noreply.github.com>
2024-06-05 16:16:12 +0300

    ДЗ8

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const nameInput = document.querySelector("#name-input");
-const nameOutput = document.querySelector("#name-output");
-nameInput.addEventListener("input", onInputCange);
-function onInputCange() {
-  const trimmedInput = nameInput.value.trim();
-  nameOutput.textContent = trimmedInput || "Anonymous";
-}
